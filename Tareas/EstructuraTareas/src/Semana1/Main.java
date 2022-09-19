8e6999f88afeef788247d7b55695238df76fd8ef
jorge09ha <jorge09ha@gmail.com>
2022-09-19 14:03:17 -0600

    Reacomodo de los Folders

---
+++ Tareas/EstructuraTareas/src/Semana1/Main.java
@@ -0,0 +1,16 @@
+package Semana1;
+
+/**
+ *
+ * @author jorge
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
