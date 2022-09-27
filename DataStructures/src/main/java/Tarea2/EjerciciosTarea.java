d514e103c23488181dda4a90b32cdebd4ee5f0aa
jorge09ha <jorge09ha@gmail.com>
2022-09-27 12:00:28 -0600

    Correcciones y ejercicio 5 listo
    Se corrige y mejora el ejercicio 4.
    El ejercicio uno se completa y se hacen dos variantes, una para ingresar el valor de la tabla de multiplicar y otro para que muestre las tablas del 0 al 9.

--- DataStructures/src/main/java/Tarea2/EjerciciosTarea.java
+++ DataStructures/src/main/java/Tarea2/EjerciciosTarea.java
@@ -47,25 +47,63 @@ public class EjerciciosTarea {
      */
     /**
      * 4.Escriba un programa que sume N números recursivamente y muestre el
-     * resultado.
+     * sumatoria.
      *
      * @param numeros Arrelo con los numeros
      * @param i Indice
-     * @param resultado resultado de la sumatoria
-     * @return se retorna el resultado
+     * @param sumatoria sumatoria de la sumatoria
+     * @return se retorna el sumatoria
      */
-    public static void ejercicio4(int[] numeros, int i, int resultado) {
+    public static int ejercicio4(int[] numeros, int i, int sumatoria) {
         if (i < numeros.length) {
-            resultado = numeros[i] + resultado;
-            System.out.println(numeros[i]+"+"+resultado);
-            ejercicio4(numeros, i + 1, resultado);
-        } else {
-            System.out.println("El resultado es: " + resultado);
+            sumatoria = numeros[i] + sumatoria;
+            sumatoria = ejercicio4(numeros, i + 1, sumatoria);
+            return sumatoria;
+        }
+        return sumatoria;
+    }
+
+    /**
+     * 5.Escriba un programa que permita, mediante la utilización de
+     * recursividad, mostrar las tablas de multiplicar.
+     *
+     * @param multiplicando
+     * @param multiplicador
+     */
+    public static void ejercicio5(int multiplicando, int multiplicador) {
+        int resultado;
+        if (multiplicador <= 10) {
+            resultado = multiplicando * multiplicador;
+            System.out.println(multiplicando + " x " + multiplicador + " = " + resultado);
+            ejercicio5(multiplicando, multiplicador + 1);
         }
     }
+
     /**
-     * 5. Escriba un programa que permita, mediante la utilización de
-     * recursividad, mostrar las tablas de multiplicar.
+     * Variante del ejercicio 5, en este caso imprime las maximo hasta la del 9.
+     *
+     * @param factor1 es multiplicando
+     * @param factor2 es multiplicador
      */
+    public static void ejercicio5_Ej2(int factor1, int factor2) {
+        int resultado;
+
+        if (factor1 < 10) {
+            if (factor2 <= 10) {
+                resultado = factor1 * factor2;
+                System.out.println(factor1 + " x " + factor2 + " = " + resultado);//PRINT---------------------
+                ejercicio5_Ej2(factor1, factor2 + 1);
+            } else {
+                factor2 = 0;
+                factor1 = factor1 + 1;
+                if (factor1 < 10) {
+                    System.out.println("\nTabla del " + factor1);
+                }
+                ejercicio5_Ej2(factor1, factor2);
+            }
+        } else {
+            System.out.println("FIN");
+        }
+    }
 
 }
--- DataStructures/src/main/java/Tarea2/Main.java
+++ DataStructures/src/main/java/Tarea2/Main.java
@@ -1,8 +1,10 @@
 package Tarea2;
 
+import java.util.Scanner;
+
 /**
  *
- * @author jorge
+ * @author jorge.hernandez
  */
 public class Main {
 
@@ -11,25 +13,33 @@ public class Main {
      */
     public static void main(String[] args) {
         //------------------------------Ejercicio 1 --------------------
-//        Integer num = 123;
-//
-//        System.out.println("Ejercicio 1" + "\n" + "Numero Original: " + num);
-//        System.out.println("Numero Invertido: " + EjerciciosTarea.ejercicio1(num));
+        Integer num = 123;
 
-        //------------------------------Ejercicio 2 --------------------
-//        String palabra = "Hola este es el ejercicio 2";
-//
-//        System.out.println("Ejercicio 2" + "\n" + "Palabra: " + palabra);
-//        EjerciciosTarea.ejercicio2(palabra, 0);
+        System.out.println("-".repeat(40) + "\n" + "Ejercicio 1" + "\n" + "Original: " + num);
+        System.out.println("Invertido: " + EjerciciosTarea.ejercicio1(num));
+
+//        //------------------------------Ejercicio 2 --------------------
+        String palabra = "Hola este es el ejercicio 2";
+
+        System.out.println("-".repeat(40) + "\n" + "Ejercicio 2" + "\n" + "String: " + palabra);
+        EjerciciosTarea.ejercicio2(palabra, 0);
 
         //------------------------------Ejercicio 3 --------------------
         //------------------------------Ejercicio 4 --------------------
-        
-        int arr[] = {5, 5, 5, 5, 5, 10};
-        System.out.println("Ejercicio 4");
-        EjerciciosTarea.ejercicio4(arr, 0, 0);
+        int arr[] = {5, 5, 5, 10, 5, 10};
+        System.out.println("-".repeat(40) + "\n" + "Ejercicio 4");
+        System.out.println("Resultado es: " + EjerciciosTarea.ejercicio4(arr, 0, 0));
 
         //------------------------------Ejercicio 5 --------------------
+        System.out.println("-".repeat(40) + "\n" + "Ejercicio 5");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el numero multiplicador: ");
+        int multiplicando = Integer.parseInt(sc.nextLine());
+        EjerciciosTarea.ejercicio5(multiplicando, 0);
+
+        //Variante del ejercicio 5
+        EjerciciosTarea.ejercicio5_Ej2(0, 0);
+
     }
 
 }
